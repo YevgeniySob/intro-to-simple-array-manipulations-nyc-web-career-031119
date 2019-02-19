e2009a7db20d186e7c38c9163dec4e51cc0dfa87
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-19 00:20:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,7 +10,10 @@ def using_pop(arr, str)
   arr.pop(str)
 end
 
-def using_unshift(arr, str)
-  arr.unshift(str)
+def pop_with_args(arr, str)
+  str = ""
+  str << arr.pop
+  str << arr.pop
+  return str
 end
 
